9f53bc12339036068b5da2ff404d546a969b79ed
Anthony Salemo <ajssalemo@gmail.com>
2022-08-03 17:22:02 -0400

    testing

--- deploy.cmd
+++ deploy.cmd
@@ -95,10 +95,15 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 )
 
 
-:: 3. Install npm packages
+echo "Installing yarn.."
+call :ExecuteCmd !NPM_CMD! install yarn -g
+
+:: 4. Install Yarn packages
+echo Installing Yarn Packages.
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  echo "Running yarn install --production.."
+  call :ExecuteCmd yarn install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
