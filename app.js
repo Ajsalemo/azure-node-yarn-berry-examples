9cbcd3be3e590a19a79d37185491561ad75b3bdb
Anthony Salemo <ajssalemo@gmail.com>
2022-08-05 19:11:49 -0400

    test

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 1337;
 http.createServer(function(_req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   // Write something that leverages a yarn installed package.
-  res.end('Hello World, v.2.5 - with Yarn Berry \n' + moment().format());
+  res.end('Hello World, v.2.6 - with Yarn v3 \n' + moment().format());
 }).listen(port);
